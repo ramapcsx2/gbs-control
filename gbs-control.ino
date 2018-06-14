20006c0738f2c6f076e0658f12c7bff5d00e48f8
Robert Neumann <mail.ramadev@gmail.com>
2018-06-14 17:43:17 +0200

    Development (#33)
    * alternative active frametime lock option
    
    * web ui: reorganization, tweaks, more preset groups

--- gbs-control.ino
+++ gbs-control.ino
@@ -36,16 +36,16 @@ extern "C" {
 #define DEBUG_IN_PIN D6 // marked "D12/MISO/D6" (Wemos D1) or D6 (Lolin NodeMCU)
 // SCL = D1 (Lolin), D15 (Wemos D1) // ESP8266 Arduino default map: SCL
 // SDA = D2 (Lolin), D14 (Wemos D1) // ESP8266 Arduino default map: SDA
-#define LEDON  pinMode(LED_BUILTIN, OUTPUT); digitalWrite(LED_BUILTIN, LOW); // active low
-#define LEDOFF digitalWrite(LED_BUILTIN, HIGH); pinMode(LED_BUILTIN, INPUT);
+#define LEDON  { pinMode(LED_BUILTIN, OUTPUT); digitalWrite(LED_BUILTIN, LOW); }// active low
+#define LEDOFF { digitalWrite(LED_BUILTIN, HIGH); pinMode(LED_BUILTIN, INPUT); }
 
 // fast ESP8266 digitalRead (21 cycles vs 77), *should* work with all possible input pins
 // but only "D7" and "D6" have been tested so far
 #define digitalRead(x) ((GPIO_REG_READ(GPIO_IN_ADDRESS) >> x) & 1)
 
 #else // Arduino
-#define LEDON  pinMode(LED_BUILTIN, OUTPUT); digitalWrite(LED_BUILTIN, HIGH);
-#define LEDOFF digitalWrite(LED_BUILTIN, LOW); pinMode(LED_BUILTIN, INPUT);
+#define LEDON  { pinMode(LED_BUILTIN, OUTPUT); digitalWrite(LED_BUILTIN, HIGH); }
+#define LEDOFF { digitalWrite(LED_BUILTIN, LOW); pinMode(LED_BUILTIN, INPUT); }
 #define DEBUG_IN_PIN 11
 
 #include "fastpin.h"
@@ -164,6 +164,7 @@ struct userOptions {
   uint8_t presetPreference; // 0 - normal, 1 - feedback clock, 2 - customized, 3 - 720p
   uint8_t presetGroup;
   uint8_t enableFrameTimeLock;
+  uint8_t frameTimeLockMethod;
 } uopts;
 struct userOptions *uopt = &uopts;
 
@@ -1725,6 +1726,7 @@ void setup() {
   uopt->presetPreference = 0; // normal, 720p, fb or custom
   uopt->presetGroup = 0; //
   uopt->enableFrameTimeLock = 0; // permanently adjust frame timing to avoid glitch vertical bar. does not work on all displays!
+  uopt->frameTimeLockMethod = 0; // compatibility with more displays
   // run time options
   rto->allowUpdatesOTA = false; // ESP over the air updates. default to off, enable via web interface
   rto->syncLockEnabled = true;  // automatically find the best horizontal total pixel value for a given input timing
@@ -1765,28 +1767,34 @@ void setup() {
       uopt->presetPreference = 0;
       uopt->enableFrameTimeLock = 0;
       uopt->presetGroup = 0;
+      uopt->frameTimeLockMethod = 0;
       saveUserPrefs(); // if this fails, there must be a spiffs problem
     }
     else {
       SerialM.println("userprefs open ok");
       //on a fresh / spiffs not formatted yet MCU:
       //userprefs.txt open ok //result[0] = 207 //result[1] = 207
-      char result[3];
-      result[0] = f.read();
-      result[0] -= '0'; // file streams with their chars..
+      char result[4];
+      result[0] = f.read(); result[0] -= '0'; // file streams with their chars..
       uopt->presetPreference = (uint8_t)result[0]; // normal, fb or custom preset
       SerialM.print("presetPreference = "); SerialM.println(uopt->presetPreference);
       if (uopt->presetPreference > 3) uopt->presetPreference = 0; // fresh spiffs ?
-      result[1] = f.read();
-      result[1] -= '0';
+
+      result[1] = f.read(); result[1] -= '0';
       uopt->enableFrameTimeLock = (uint8_t)result[1]; // Frame Time Lock
       SerialM.print("FrameTime Lock = "); SerialM.println(uopt->enableFrameTimeLock);
       if (uopt->enableFrameTimeLock > 1) uopt->enableFrameTimeLock = 0; // fresh spiffs ?
-      result[2] = f.read();
-      result[2] -= '0';
+
+      result[2] = f.read(); result[2] -= '0';
       uopt->presetGroup = (uint8_t)result[2];
       SerialM.print("presetGroup = "); SerialM.println(uopt->presetGroup); // custom preset group
-      if (uopt->presetGroup > 5) uopt->presetGroup = 0;
+      if (uopt->presetGroup > 4) uopt->presetGroup = 0;
+
+      result[3] = f.read(); result[3] -= '0';
+      uopt->frameTimeLockMethod = (uint8_t)result[3];
+      SerialM.print("frameTimeLockMethod = "); SerialM.println(uopt->frameTimeLockMethod);
+      if (uopt->frameTimeLockMethod > 1) uopt->frameTimeLockMethod = 0;
+
       f.close();
     }
   }
@@ -2421,7 +2429,7 @@ void loop() {
     readFromRegister(0x63, 1, &debugRegBackup);
     writeOneByte(0x63, 0x0f);
     lastVsyncLock = millis();
-    if (!FrameSync::run()) {
+    if (!FrameSync::run(uopt->frameTimeLockMethod)) {
       if (rto->syncLockFailIgnore-- == 0) {
         FrameSync::reset(); // in case run() failed because we lost a sync signal
       }
@@ -2620,18 +2628,21 @@ void handleType2Command() {
         saveUserPrefs();
         break;
       case '2':
-        uopt->presetPreference = 2; // custom
-        saveUserPrefs();
+        //
         break;
       case '3':
         {
-          const uint8_t* preset = loadPresetFromSPIFFS(rto->videoStandardInput); // load for current video mode
-          writeProgramArrayNew(preset);
-          doPostPresetLoadSteps();
+          if (rto->videoStandardInput == 0) SerialM.println("no input detected, aborting action");
+          else {
+            const uint8_t* preset = loadPresetFromSPIFFS(rto->videoStandardInput); // load for current video mode
+            writeProgramArrayNew(preset);
+            doPostPresetLoadSteps();
+          }
         }
         break;
       case '4':
-        savePresetToSPIFFS();
+        if (rto->videoStandardInput == 0) SerialM.println("no input detected, aborting action");
+        else savePresetToSPIFFS();
         break;
       case '5':
         //Frame Time Lock ON
@@ -2681,14 +2692,27 @@ void handleType2Command() {
         break;
       case 'b':
         uopt->presetGroup = 0;
+        uopt->presetPreference = 2; // custom
         saveUserPrefs();
         break;
       case 'c':
         uopt->presetGroup = 1;
+        uopt->presetPreference = 2;
         saveUserPrefs();
         break;
       case 'd':
         uopt->presetGroup = 2;
+        uopt->presetPreference = 2;
+        saveUserPrefs();
+        break;
+      case 'j':
+        uopt->presetGroup = 3;
+        uopt->presetPreference = 2;
+        saveUserPrefs();
+        break;
+      case 'k':
+        uopt->presetGroup = 4;
+        uopt->presetPreference = 2;
         saveUserPrefs();
         break;
       case 'e':
@@ -2696,6 +2720,23 @@ void handleType2Command() {
           Dir dir = SPIFFS.openDir("/");
           while (dir.next()) {
             SerialM.print(dir.fileName()); SerialM.print(" "); SerialM.println(dir.fileSize());
+          }
+          ////
+          File f = SPIFFS.open("/userprefs.txt", "r");
+          if (!f) {
+            SerialM.println("userprefs open failed");
+          }
+          else {
+            char result[4];
+            result[0] = f.read(); result[0] -= '0'; // file streams with their chars..
+            SerialM.print("presetPreference = "); SerialM.println((uint8_t)result[0]);
+            result[1] = f.read(); result[1] -= '0';
+            SerialM.print("FrameTime Lock = "); SerialM.println((uint8_t)result[1]);
+            result[2] = f.read(); result[2] -= '0';
+            SerialM.print("presetGroup = "); SerialM.println((uint8_t)result[2]);
+            result[3] = f.read(); result[3] -= '0';
+            SerialM.print("frameTimeLockMethod = "); SerialM.println((uint8_t)result[3]);
+            f.close();
           }
         }
         break;
@@ -2732,6 +2773,12 @@ void handleType2Command() {
           uopt->presetPreference = backup;
         }
         break;
+      case 'i':
+        // toggle active frametime lock method
+        if (uopt->frameTimeLockMethod == 0) uopt->frameTimeLockMethod = 1;
+        else if (uopt->frameTimeLockMethod == 1) uopt->frameTimeLockMethod = 0;
+        saveUserPrefs();
+        break;
       default:
         break;
     }
@@ -2749,7 +2796,6 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t lenght
       }
       break;
     case WStype_TEXT:                     // if new text data is received
-      LEDON delay(1); LEDOFF
       //SerialM.printf("[%u] get Text (length: %d): %s\n", num, lenght, payload);
       break;
     default:
@@ -2829,7 +2875,7 @@ void StrClear(char *str, uint16_t length)
   }
 }
 
-const uint8_t* loadPresetFromSPIFFS(byte result) {
+const uint8_t* loadPresetFromSPIFFS(byte forVideoMode) {
   static uint8_t preset[592];
   String s = "";
   char group = '0';
@@ -2850,38 +2896,37 @@ const uint8_t* loadPresetFromSPIFFS(byte result) {
   else {
     // file not found, we don't know what preset to load
     SerialM.println("please select a preset group first!");
-    if (result == 2 || result == 4) return pal_240p;
+    if (forVideoMode == 2 || forVideoMode == 4) return pal_240p;
     else return ntsc_240p;
   }
 
-  if (result == 1) {
+  if (forVideoMode == 1) {
     f = SPIFFS.open("/preset_ntsc." + String(group), "r");
   }
-  else if (result == 2) {
+  else if (forVideoMode == 2) {
     f = SPIFFS.open("/preset_pal." + String(group), "r");
   }
-  else if (result == 3) {
+  else if (forVideoMode == 3) {
     f = SPIFFS.open("/preset_ntsc_480p." + String(group), "r");
   }
-  else if (result == 4) {
+  else if (forVideoMode == 4) {
     f = SPIFFS.open("/preset_pal_576p." + String(group), "r");
   }
-  else if (result == 5) {
+  else if (forVideoMode == 5) {
     f = SPIFFS.open("/preset_ntsc_720p." + String(group), "r");
   }
-  else if (result == 6) {
+  else if (forVideoMode == 6) {
     f = SPIFFS.open("/preset_ntsc_1080p." + String(group), "r");
   }
 
   if (!f) {
     SerialM.println("open preset file failed");
-    if (result == 2 || result == 4) return pal_240p;
+    if (forVideoMode == 2 || forVideoMode == 4) return pal_240p;
     else return ntsc_240p;
   }
   else {
     SerialM.println("preset file open ok: ");
-    SerialM.print(f.name());
-    SerialM.print(" "); SerialM.println(f.size());
+    SerialM.println(f.name());
     s = f.readStringUntil('}');
     f.close();
   }
@@ -3006,8 +3051,27 @@ void saveUserPrefs() {
   f.write(uopt->presetPreference + '0');
   f.write(uopt->enableFrameTimeLock + '0');
   f.write(uopt->presetGroup + '0');
-  SerialM.println("userprefs saved");
+  f.write(uopt->frameTimeLockMethod + '0');
+  SerialM.println("userprefs saved: ");
   f.close();
+
+  // print results
+  f = SPIFFS.open("/userprefs.txt", "r");
+  if (!f) {
+    SerialM.println("userprefs open failed");
+  }
+  else {
+    char result[4];
+    result[0] = f.read(); result[0] -= '0'; // file streams with their chars..
+    SerialM.print("  presetPreference = "); SerialM.println((uint8_t)result[0]);
+    result[1] = f.read(); result[1] -= '0';
+    SerialM.print("  FrameTime Lock = "); SerialM.println((uint8_t)result[1]);
+    result[2] = f.read(); result[2] -= '0';
+    SerialM.print("  presetGroup = "); SerialM.println((uint8_t)result[2]);
+    result[3] = f.read(); result[3] -= '0';
+    SerialM.print("  frameTimeLockMethod = "); SerialM.println((uint8_t)result[3]);
+    f.close();
+  }
 }
 
 #endif
