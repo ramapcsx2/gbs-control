2fda81d9d93cfb57a6f3b3fd26a43fdb4a325652
xps3riments <xps3riments@gmail.com>
2021-05-12 11:54:21 +0200

    Fix wrong Serial Output

--- gbs-control.ino
+++ gbs-control.ino
@@ -8673,11 +8673,11 @@ void handleType2Command(char argument) {
     SerialM.print(F("pal force 60hz "));
     if (uopt->PalForce60 == 0) {
       uopt->PalForce60 = 1;
-      Serial.println("on");
+      SerialM.println("on");
     }
     else {
       uopt->PalForce60 = 0;
-      Serial.println("off");
+      SerialM.println("off");
     }
     saveUserPrefs();
     break;
