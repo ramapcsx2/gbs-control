b30a1e942fcbfe400d9d438888137ab1e07096f2
Florian <Metaln00b@web.de>
2023-10-22 03:38:02 +0200

    Add platformio with small impact

--- public/scripts/html2h.sh
+++ public/scripts/html2h.sh
@@ -2,6 +2,6 @@
 
 cd ../..
 gzip -c9 webui.html > webui_html && xxd -i webui_html > webui_html.h && rm webui_html && sed -i -e 's/unsigned char webui_html\[]/const uint8_t webui_html[] PROGMEM/' webui_html.h && sed -i -e 's/unsigned int webui_html_len/const unsigned int webui_html_len/' webui_html.h
-rm webui_html.h-e
+rm -fv webui_html.h-e
 
 echo "webui_html.h GENERATED";
